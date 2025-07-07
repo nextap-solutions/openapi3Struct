556935cb9a6ed861fb5ce21aa5b41835bc26181a
Diego Portillo <dapadxdxd@gmail.com>
2025-07-07 14:13:24 +0200

    new helped and domain

---
+++ domain/endpoint.go
@@ -0,0 +1,239 @@
+package domain
+
+import (
+	"fmt"
+	"net/http"
+	"reflect"
+
+	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/nextap-solutions/openapi3Struct"
+)
+
+func GetTypeName(v any) string {
+	if v == nil {
+		return ""
+	}
+	typ := reflect.TypeOf(v)
+	if typ.Kind() == reflect.Ptr {
+		typ = typ.Elem()
+	}
+	return typ.Name()
+}
+
+func ToPointer[T any](v T) *T {
+	return &v
+}
+
+type OperationBuilder struct {
+	op        *openapi3.Operation
+	responses map[int]*openapi3.ResponseRef
+}
+
+func NewOperationBuilder() *OperationBuilder {
+	return &OperationBuilder{
+		op:        &openapi3.Operation{},
+		responses: make(map[int]*openapi3.ResponseRef),
+	}
+}
+
+func (ob *OperationBuilder) WithTags(tags ...string) *OperationBuilder {
+	ob.op.Tags = tags
+	return ob
+}
+
+func (ob *OperationBuilder) WithDescription(desc string) *OperationBuilder {
+	ob.op.Description = desc
+	return ob
+}
+
+func (ob *OperationBuilder) WithRequestBodyType(bodyType any, description string, required bool) *OperationBuilder {
+	if bodyType == nil {
+		fmt.Println("Warning: bodyType is nil for request body.")
+		ob.op.RequestBody = &openapi3.RequestBodyRef{
+			Value: &openapi3.RequestBody{
+				Required:    required,
+				Description: description,
+				Content: map[string]*openapi3.MediaType{
+					"application/json": {
+						Schema: openapi3.NewSchemaRef("", nil),
+					},
+				},
+			},
+		}
+		return ob
+	}
+
+	typ := reflect.TypeOf(bodyType)
+	isSlice := typ.Kind() == reflect.Slice
+	var itemTypeName string
+	var schemaRef *openapi3.SchemaRef
+
+	if isSlice {
+		elemType := typ.Elem()
+		if elemType.Kind() == reflect.Ptr {
+			elemType = elemType.Elem()
+		}
+		itemTypeName = elemType.Name()
+		if itemTypeName == "" {
+			fmt.Printf("Warning: Could not determine element type name for request body array: %T\n", bodyType)
+			return ob
+		}
+		schemaRef = &openapi3.SchemaRef{
+			Value: &openapi3.Schema{
+				Type: openapi3.NewArraySchema().Type,
+				Items: &openapi3.SchemaRef{
+					Ref: fmt.Sprintf("#/components/schemas/%s", itemTypeName),
+				},
+			},
+		}
+	} else {
+		itemTypeName = GetTypeName(bodyType)
+		if itemTypeName == "" {
+			fmt.Printf("Warning: Could not determine type name for request body: %T\n", bodyType)
+			return ob
+		}
+		schemaRef = &openapi3.SchemaRef{
+			Ref: fmt.Sprintf("#/components/schemas/%s", itemTypeName),
+		}
+	}
+
+	ob.op.RequestBody = &openapi3.RequestBodyRef{
+		Value: &openapi3.RequestBody{
+			Required:    required,
+			Description: description,
+			Content: map[string]*openapi3.MediaType{
+				"application/json": {
+					Schema: schemaRef,
+				},
+			},
+		},
+	}
+	return ob
+}
+
+func (ob *OperationBuilder) WithResponse(statusCode int, description string, responseType any) *OperationBuilder {
+	var schemaRef *openapi3.SchemaRef
+	var content map[string]*openapi3.MediaType
+
+	if responseType != nil {
+		typeName := GetTypeName(responseType)
+		if typeName == "" {
+			fmt.Printf("Warning: Could not determine type name for response type: %T\n", responseType)
+			schemaRef = openapi3.NewSchemaRef("", nil) // Empty schema
+		} else {
+			schemaRef = openapi3.NewSchemaRef(fmt.Sprintf("#/components/schemas/%s", typeName), nil)
+		}
+		content = map[string]*openapi3.MediaType{
+			"application/json": {
+				Schema: schemaRef,
+			},
+		}
+	}
+
+	ob.responses[statusCode] = &openapi3.ResponseRef{
+		Value: &openapi3.Response{
+			Description: ToPointer(description),
+			Content:     content,
+		},
+	}
+	return ob
+}
+
+func (ob *OperationBuilder) WithParameter(name, in, description string, required bool, schemaType any) *OperationBuilder {
+	param := &openapi3.Parameter{
+		Name:        name,
+		In:          in,
+		Description: description,
+		Required:    required,
+	}
+
+	// WIP - it'll be improved once i start working on hermes
+	if schemaType != nil {
+		typeName := GetTypeName(schemaType)
+		var paramSchema *openapi3.Schema
+		if typeName == "" {
+			switch reflect.TypeOf(schemaType).Kind() {
+			case reflect.String:
+				paramSchema = openapi3.NewStringSchema()
+			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+				paramSchema = openapi3.NewInt64Schema()
+			case reflect.Float32, reflect.Float64:
+				paramSchema = openapi3.NewFloat64Schema()
+			case reflect.Bool:
+				paramSchema = openapi3.NewBoolSchema()
+			default:
+				fmt.Printf("Warning: Could not determine primitive OpenAPI schema for parameter '%s' type: %T. Schema will be empty.\n", name, schemaType)
+				paramSchema = openapi3.NewSchema()
+			}
+			param.Schema = openapi3.NewSchemaRef("", paramSchema)
+		} else {
+			param.Schema = openapi3.NewSchemaRef(fmt.Sprintf("#/components/schemas/%s", typeName), nil)
+		}
+	} else {
+		param.Schema = openapi3.NewSchemaRef("", nil)
+	}
+
+	ob.op.Parameters = append(ob.op.Parameters, &openapi3.ParameterRef{
+		Value: param,
+	})
+	return ob
+}
+
+func (ob *OperationBuilder) WithParameterRef(ref string) *OperationBuilder {
+	ob.op.Parameters = append(ob.op.Parameters, &openapi3.ParameterRef{
+		Ref: ref,
+	})
+	return ob
+}
+
+func (ob *OperationBuilder) Build() *openapi3.Operation {
+	responses := []openapi3.NewResponsesOption{}
+	for status, res := range ob.responses {
+		responses = append(responses, openapi3.WithStatus(status, res))
+	}
+	ob.op.Responses = openapi3.NewResponses(responses...)
+	return ob.op
+}
+
+type HandlerProvider interface{}
+
+type EndpointDoc struct {
+	Path     string
+	Version  int
+	Method   string
+	PathItem *OperationBuilder
+}
+
+func (ep *EndpointDoc) GetPath() string {
+	path := ""
+	if ep.Version > 0 {
+		path = fmt.Sprintf("/v%d/%s", ep.Version, ep.Path)
+	} else {
+		path = fmt.Sprintf("/%s", ep.Path)
+	}
+
+	return path
+}
+
+func (ep *EndpointDoc) BuildOpenAPiStruct() openapi3Struct.Path {
+	item := openapi3.PathItem{}
+	op := ep.PathItem.Build()
+	switch ep.Method {
+	case http.MethodPost:
+		item.Post = op
+	case http.MethodGet:
+		item.Get = op
+	case http.MethodPut:
+		item.Put = op
+	case http.MethodDelete:
+		item.Delete = op
+	case http.MethodOptions:
+		item.Options = op
+	default:
+		panic(fmt.Sprintf("Unknown request method: %d", ep.Method))
+	}
+	return openapi3Struct.Path{
+		Path: ep.GetPath(),
+		Item: item,
+	}
+}
--- go.mod
+++ go.mod
@@ -1,25 +1,26 @@
 module github.com/nextap-solutions/openapi3Struct
 
-go 1.22.0
+go 1.23.0
 
-toolchain go1.23.4
+toolchain go1.24.4
 
-require github.com/getkin/kin-openapi v0.128.0
+require github.com/getkin/kin-openapi v0.132.0
 
 require (
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/oasdiff/yaml v0.0.0-20250309154309-f31be36b4037 // indirect
+	github.com/oasdiff/yaml3 v0.0.0-20250309153720-d2182401db90 // indirect
 	github.com/perimeterx/marshmallow v1.1.5 // indirect
-	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/sync v0.15.0 // indirect
 )
 
 require (
-	github.com/go-openapi/jsonpointer v0.21.0 // indirect
-	github.com/go-openapi/swag v0.23.0 // indirect
-	github.com/invopop/yaml v0.3.1 // indirect
+	github.com/go-openapi/jsonpointer v0.21.1 // indirect
+	github.com/go-openapi/swag v0.23.1 // indirect
 	github.com/itchyny/json2yaml v0.1.4
 	github.com/mailru/easyjson v0.9.0 // indirect
-	golang.org/x/mod v0.22.0 // indirect
-	golang.org/x/tools v0.29.0
+	golang.org/x/mod v0.25.0 // indirect
+	golang.org/x/tools v0.34.0
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
